5c3c024f1bd0d85ab41ec2603943f5d619ca3e7c
albertodeste <alberto.deste@larus-ba.it>
2017-05-21 03:35:22 +0200

    :recycle: Refactoring of get methods, removed cypher queries

--- src/main/java/org/homer/graph/versioner/procedure/Get.java
+++ src/main/java/org/homer/graph/versioner/procedure/Get.java
@@ -2,15 +2,13 @@ package org.homer.graph.versioner.procedure;
 
 import org.homer.graph.versioner.output.NodeOutput;
 import org.homer.graph.versioner.output.PathOutput;
+import org.neo4j.cypher.internal.compiler.v3_2.commands.expressions.PathValueBuilder;
 import org.neo4j.graphdb.*;
-import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Spliterator;
+import java.util.*;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -22,15 +20,16 @@ import static org.neo4j.procedure.Mode.DEFAULT;
  */
 public class Get {
 
-    @Context
-    public GraphDatabaseService db;
-
     @Procedure(value = "graph.versioner.get.current.path", mode = DEFAULT)
     @Description("graph.versioner.get.current.path(entity) - Get a the current Path (Entity, State and rels) for the given Entity.")
     public Stream<PathOutput> getCurrentPath(
             @Name("entity") Node entity) {
 
-        return getCurrentPathResourceIterator(entity).stream().map(PathOutput::new);
+		PathValueBuilder builder = new PathValueBuilder().addNode(entity);
+
+		Optional.ofNullable(entity.getSingleRelationship(RelationshipType.withName(CURRENT_TYPE), Direction.OUTGOING)).map(builder::addOutgoingRelationship);
+
+        return Stream.of(builder.result()).map(PathOutput::new);
     }
 
     @Procedure(value = "graph.versioner.get.current.state", mode = DEFAULT)
@@ -38,8 +37,8 @@ public class Get {
     public Stream<NodeOutput> getCurrentState(
             @Name("entity") Node entity) {
 
-        Spliterator<Relationship> iterator = entity.getRelationships(RelationshipType.withName(CURRENT_TYPE), Direction.OUTGOING).spliterator();
-        return StreamSupport.stream(iterator, false).map(currentState -> new NodeOutput(currentState.getEndNode()));
+        return Stream.of(Optional.ofNullable(entity.getSingleRelationship(RelationshipType.withName(CURRENT_TYPE), Direction.OUTGOING))
+				.map(Relationship::getEndNode).map(NodeOutput::new).orElse(null));
     }
 
     @Procedure(value = "graph.versioner.get.all", mode = DEFAULT)
@@ -47,26 +46,17 @@ public class Get {
     public Stream<PathOutput> getAllState(
             @Name("entity") Node entity) {
 
-        ResourceIterator<Path> result;
+		PathValueBuilder builder = new PathValueBuilder();
+		builder.addNode(entity);
+		builder.addOutgoingRelationship(entity.getSingleRelationship(RelationshipType.withName(CURRENT_TYPE), Direction.OUTGOING));
+		StreamSupport.stream(entity.getRelationships(RelationshipType.withName(HAS_STATE_TYPE), Direction.OUTGOING).spliterator(), false)
+				//.sorted((a, b) -> -1 * Long.compare((long)a.getProperty(START_DATE_PROP), (long)b.getProperty(START_DATE_PROP)))
+				.forEach(rel ->
+					Optional.ofNullable(rel.getEndNode().getSingleRelationship(RelationshipType.withName(PREVIOUS_TYPE), Direction.OUTGOING))
+							.map(builder::addOutgoingRelationship)
+		);
 
-        Spliterator<Relationship> hasStateRelsIterator = entity.getRelationships(RelationshipType.withName(HAS_STATE_TYPE), Direction.OUTGOING).spliterator();
-
-        if (StreamSupport.stream(hasStateRelsIterator, false).count() != 1) {
-            String query = "MATCH path=(e)-[%s]->(:%s)-[:%s*]->(:%s) WHERE id(e)=%d " +
-                    "RETURN path, length(path) ORDER BY length(path) DESC LIMIT 1";
-
-            result = this.db.execute(String.format(query,
-                    CURRENT_TYPE,
-                    STATE_LABEL,
-                    PREVIOUS_TYPE,
-                    STATE_LABEL,
-                    entity.getId()
-            )).columnAs("path");
-        } else {
-            result = getCurrentPathResourceIterator(entity);
-        }
-
-        return result.stream().map(PathOutput::new);
+		return Stream.of(new PathOutput(builder.result()));
     }
 
     @Procedure(value = "graph.versioner.get.by.label", mode = DEFAULT)
@@ -75,19 +65,10 @@ public class Get {
             @Name("entity") Node entity,
             @Name("label") String label) {
 
-        Collection<Node> result = new ArrayList<>();
-
-        Spliterator<Relationship> hasStateRelsIterator = entity.getRelationships(RelationshipType.withName(HAS_STATE_TYPE), Direction.OUTGOING).spliterator();
-
-        StreamSupport.stream(hasStateRelsIterator, false).forEach(relationship -> {
-            Node state = relationship.getEndNode();
-
-            if (state.hasLabel(Label.label(label))) {
-                result.add(state);
-            }
-        });
-
-        return result.parallelStream().map(NodeOutput::new);
+		return StreamSupport.stream(entity.getRelationships(RelationshipType.withName(HAS_STATE_TYPE), Direction.OUTGOING).spliterator(), false)
+				.map(Relationship::getEndNode)
+				.filter(node -> node.hasLabel(Label.label(label)))
+				.map(NodeOutput::new);
     }
 
     @Procedure(value = "graph.versioner.get.by.date", mode = DEFAULT)
@@ -96,34 +77,9 @@ public class Get {
             @Name("entity") Node entity,
             @Name("date") long date) {
 
-        Collection<Node> result = new ArrayList<>();
-
-        Spliterator<Relationship> hasStateRelsIterator = entity.getRelationships(RelationshipType.withName(HAS_STATE_TYPE), Direction.OUTGOING).spliterator();
-
-        StreamSupport.stream(hasStateRelsIterator, false).forEach(relationship -> {
-            Node state = relationship.getEndNode();
-
-            if (relationship.getProperty("startDate").equals(date)) {
-                result.add(state);
-            }
-        });
-
-        return result.parallelStream().map(NodeOutput::new);
-    }
-
-    /**
-     * Returns a {@link ResourceIterator<Path>} with the current {@link Path}}
-     *
-     * @param node a {@link Node} representing the current State
-     * @return a {@link ResourceIterator<Path>} with the current {@link Path}
-     */
-    private ResourceIterator<Path> getCurrentPathResourceIterator(Node node) {
-        String query = "MATCH path=(e)-[c:%s]->(s:%s) WHERE id(e)=%d RETURN path";
-
-        return this.db.execute(String.format(query,
-                CURRENT_TYPE,
-                STATE_LABEL,
-                node.getId()
-        )).columnAs("path");
+		return StreamSupport.stream(entity.getRelationships(RelationshipType.withName(HAS_STATE_TYPE), Direction.OUTGOING).spliterator(), false)
+				.filter(relationship -> relationship.getProperty(START_DATE_PROP).equals(date))
+				.map(Relationship::getEndNode)
+				.map(NodeOutput::new);
     }
 }
